54931ad0cce5804dca5342c063d1445cd2f24859
mmaffer <maria.moya@tecsup.edu.pe>
2025-03-26 08:49:34 -0500

    Serie de Fibonacci

--- semana2/ejercicio1.py
+++ semana2/ejercicio1.py
@@ -1,1 +1,12 @@
-print("Holap")
+def fibonacci(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
+# Casos de prueba
+print("fibonacci(0) =", fibonacci(0))     # 0
+print("fibonacci(1) =", fibonacci(1))     # 1
+print("fibonacci(2) =", fibonacci(2))     # 1
